f77ab4ee0ca98d358353e0d6fcb3e945ec7e3eb8
vishal-rankhamb <vrankhamb9@gmail.com>
2023-06-04 15:47:53 +0530

    Second Commit

--- Begin.java
+++ Begin.java
@@ -2,5 +2,6 @@ Class Begin{
 	public static void main(String[] args){
 		System.out.println("Hello");
 		System.out.println("World");
+		My Story getting published.So I going to move further and move around her home.
 	}
 }
